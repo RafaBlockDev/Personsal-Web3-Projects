46c96228ed7645f4db3da5475da7d84ae9bd992b
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-09-05 09:34:17 -0500

    Adding subgraph

--- projects/ERC721/contracts/ERC721.sol
+++ projects/ERC721/contracts/ERC721.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
-    constructor() ERC721("NFT", "MEXA") {
-        _mint(msg.sender, 1);
+    uint256 price = 1 * 10**18 ether;
+
+    constructor() ERC721("NFT", "MEXA") {}
+
+    function mint(address to, uint256 tokenId) public {
+        require(msg.balance => price, "The price need to be 1 ether or more")
     }
+
+    
 }
