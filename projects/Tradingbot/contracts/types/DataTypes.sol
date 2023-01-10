1a9d437b803742a42bcca1295a3cc3c27b0112b2
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-01-09 20:16:48 -0600

    chore: add order in project

---
+++ projects/Tradingbot/contracts/types/DataTypes.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+library DataTypes {
+
+    struct Value {
+        uint256 value;
+    }
+
+    struct Info {
+    
+    }
+
+    struct ActionArgs {
+        
+    }
+
+    enum ActionType {
+
+    }
+
+    enum AssetAmount {
+
+    }
+
+
+}
